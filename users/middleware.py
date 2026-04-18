c9f47fe87325e5ddd41004c55edf81775733d89c
sabbir <23303189@iubat.edu>
2026-04-18 23:48:05 +0600

    add patient previous diseage and doctor diseages specialities

--- users/middleware.py
+++ users/middleware.py
@@ -13,6 +13,14 @@ class LoginRequiredPathMiddleware:
             'patient/profile/',
             'profile/',
         )
+        self.doctor_only_prefixes = (
+            'doctor_dashboard/',
+            'doctor/profile/',
+        )
+        self.patient_only_prefixes = (
+            'patient_dashboard/',
+            'patient/profile/',
+        )
 
     def __call__(self, request):
         path = request.path_info.lstrip('/')
@@ -20,5 +28,12 @@ class LoginRequiredPathMiddleware:
         if not request.user.is_authenticated:
             if path.startswith(self.protected_prefixes):
                 return redirect('login')
+
+        if request.user.is_authenticated:
+            if path.startswith(self.doctor_only_prefixes) and not request.user.is_doctor:
+                return redirect('patient_dashboard')
+
+            if path.startswith(self.patient_only_prefixes) and request.user.is_doctor:
+                return redirect('doctor_dashboard')
 
         return self.get_response(request)
